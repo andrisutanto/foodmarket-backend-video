777062cf5e060e2a34849881feef3a0b90435ecd
Andri Sutanto <me@andrisutanto.com>
2022-10-08 06:52:59 +0700

    API Register User
    API Register User

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -9,9 +10,12 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use PhpParser\Node\Stmt\TryCatch;
 
 class UserController extends Controller
 {
+
+    use PasswordValidationRules
 
     public function login(Request $request)
     {
@@ -45,6 +49,48 @@ class UserController extends Controller
             ], 'Authenticated');
 
         } catch(Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            //validasi kodenya
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => $this->passwordRules()
+            ]);
+
+            //simpan ke database
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password),
+            ]);
+
+            //request kembali datanya
+            $user = User::where('email', $request->email)->first();
+
+            //create token setelah register agar langsung login
+            $tokenResult = $user->createtoken('authToken')->plainTextToken;
+
+            //beri response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]);
+
+        } catch (Exception $error) {
             return ResponseFormatter::error([
                 'message' => 'Something went wrong',
                 'error' => $error
